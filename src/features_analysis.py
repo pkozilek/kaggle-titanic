461bed1396039e62f019274ee1fcaeb412adc88d
Pedro Kozilek <pedro.kozilek@meseems.com.br>
2020-05-24 09:32:40 -0300

    Age corr analysis

--- src/features_analysis.py
+++ src/features_analysis.py
@@ -6,6 +6,11 @@ import functions.graphs as g
 features = pd.read_csv('datasets/features.csv', index_col=0)
 features_survived = features.loc[features.Survived == 1]
 features_died = features.loc[features.Survived == 0]
+features_ac = features.loc[features.Age != 0] # Age completed
+features_ac_survived = features_ac.loc[features_ac.Survived == 1]
+features__ac_died = features_ac.loc[features_ac.Survived == 0]
+
+### Bias Analysis
 
 # Age bias
 age_bias = g.histogram(
@@ -17,7 +22,6 @@ age_bias = g.histogram(
     xaxis_title='Age',
     yaxis_title='%'
 )
-age_bias.show()
 
 # Sex bias
 sex_bias = g.histogram(
@@ -29,7 +33,6 @@ sex_bias = g.histogram(
     xaxis_title='Sex',
     yaxis_title='%'
 )
-sex_bias.show()
 
 # Pclass bias
 pclass = g.histogram(
@@ -41,7 +44,6 @@ pclass = g.histogram(
     xaxis_title='Pclass',
     yaxis_title='%'
 )
-pclass.show()
 
 # SibSp bias
 sibsp = g.histogram(
@@ -53,7 +55,6 @@ sibsp = g.histogram(
     xaxis_title='SibSp',
     yaxis_title='%'
 )
-sibsp.show()
 
 # Parch bias
 parch = g.histogram(
@@ -65,7 +66,6 @@ parch = g.histogram(
     xaxis_title='Parch',
     yaxis_title='%'
 )
-parch.show()
 
 # Embarked bias
 embarked = g.histogram(
@@ -77,7 +77,6 @@ embarked = g.histogram(
     xaxis_title='Embarked',
     yaxis_title='%'
 )
-embarked.show()
 
 # Fare bias
 fare = g.histogram(
@@ -89,4 +88,87 @@ fare = g.histogram(
     xaxis_title='Fare',
     yaxis_title='%'
 )
-fare.show()
+
+# age_bias.show()
+# sex_bias.show()
+# pclass.show()
+# sibsp.show()
+# parch.show()
+# embarked.show()
+# fare.show()
+
+### Age correlation analysis
+
+age_sex_scatter = g.scatterplot(
+    x=[features_ac_survived.Age, features__ac_died.Age],
+    y=[features_ac_survived.Sex, features__ac_died.Sex],
+    labels=['Survived', 'Died'],
+    x_jitter=0.5,
+    y_jitter=0.25,
+    title='Sex x Age',
+    xaxis_title='Age',
+    yaxis_title='Sex'
+)
+
+age_pclass_scatter = g.scatterplot(
+    x=[features_ac_survived.Age, features__ac_died.Age],
+    y=[features_ac_survived.Pclass, features__ac_died.Pclass],
+    labels=['Survived', 'Died'],
+    x_jitter=0.5,
+    y_jitter=0.25,
+    title='Pclass x Age',
+    xaxis_title='Age',
+    yaxis_title='Pclass'
+)
+
+age_sibsp_scatter = g.scatterplot(
+    x=[features_ac_survived.Age, features__ac_died.Age],
+    y=[features_ac_survived.SibSp, features__ac_died.SibSp],
+    labels=['Survived', 'Died'],
+    x_jitter=0.5,
+    y_jitter=0.25,
+    title='SibSp x Age',
+    xaxis_title='Age',
+    yaxis_title='SibSp'
+)
+
+age_parch_scatter = g.scatterplot(
+    x=[features_ac_survived.Age, features__ac_died.Age],
+    y=[features_ac_survived.Parch, features__ac_died.Parch],
+    labels=['Survived', 'Died'],
+    x_jitter=0.5,
+    y_jitter=0.25,
+    title='Parch x Age',
+    xaxis_title='Age',
+    yaxis_title='Parch'
+)
+
+age_embarked_scatter = g.scatterplot(
+    x=[features_ac_survived.Age, features__ac_died.Age],
+    y=[features_ac_survived.Embarked, features__ac_died.Embarked],
+    labels=['Survived', 'Died'],
+    x_jitter=0.5,
+    y_jitter=0.25,
+    title='Embarked x Age',
+    xaxis_title='Age',
+    yaxis_title='Embarked'
+)
+
+age_fare_scatter = g.scatterplot(
+    x=[features_ac_survived.Age, features__ac_died.Age],
+    y=[features_ac_survived.Fare, features__ac_died.Fare],
+    labels=['Survived', 'Died'],
+    x_jitter=0.5,
+    y_jitter=0.25,
+    title='Fare x Age',
+    xaxis_title='Age',
+    yaxis_title='Fare'
+)
+
+
+# age_sex_scatter.show()
+# age_pclass_scatter.show()
+# age_sibsp_scatter.show()
+# age_parch_scatter.show()
+# age_embarked_scatter.show()
+# age_fare_scatter.show()
--- src/functions/graphs.py
+++ src/functions/graphs.py
@@ -1,5 +1,6 @@
 import plotly.graph_objects as go
 import numpy as np
+import random
 
 colors = ['#122C2A', '#99A697', '#143814', '#3F4416', '#3F4416', '#6B323E', '#878166', '#99A697']
 
@@ -29,3 +30,38 @@ def histogram(data_list=[], labels=[], x_bins=[], title='', xaxis_title='', yaxi
         bargroupgap=0.1
     )
     return fig    
+
+
+def scatterplot(x=[], y=[], labels=[], x_jitter=False, y_jitter=False,
+                title='', xaxis_title='', yaxis_title=''):
+    if x_jitter:
+        for i, x_data in enumerate(x):
+            x[i] = add_jitter(list(x_data), x_jitter)
+    if x_jitter:
+        for i, y_data in enumerate(y):
+            y[i] = add_jitter(list(y_data), y_jitter)
+    fig = go.Figure()
+    
+    for i, (x_data, y_data) in enumerate(zip(x, y)):
+        fig.add_trace(go.Scatter(
+            x=x_data,
+            y=y_data,
+            mode='markers',
+            marker_color=colors[i],
+            name=labels[i]
+        ))
+
+    fig.update_layout(
+        title_text=title,
+        xaxis_title_text=xaxis_title,
+        yaxis_title_text=yaxis_title,
+        showlegend=True
+    )
+    return fig
+
+
+def add_jitter(data=[], jitter=0):
+    jittered = []
+    for num in data:
+        jittered.append(num + random.uniform(jitter * (-1), jitter))
+    return jittered    
